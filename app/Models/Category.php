2420a867fc78392c05a845fc3fd026502546ce17
Mothy Tayson <moshimoshi6k@gmail.com>
2025-08-27 23:35:25 -0700

    logs working for categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,13 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 class Category extends Model
 {
     /** @use HasFactory<\Database\Factories\CategoryFactory> */
     use HasFactory;
+    use LogsActivity;
 
     protected $guarded = [];
+
+        public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['name', 'description']) // Log changes to the 'roles' relationship
+            ->logOnlyDirty() // Only log changed attributes
+            ->dontSubmitEmptyLogs(); // Prevent logging if no changes occurred
+    }
 
     public static function createMany(array $records)
     {
